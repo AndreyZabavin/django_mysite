a031b90fe1b1cba2a56936dbbafa01edc7ce6b11
Andrey <a.zabavin@yandex.ru>
2023-07-11 15:34:40 +0300

    product model description fixid

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
         ordering = ["name", "price"]
 
     name = models.CharField(max_length=100, db_index=True)
-    description = models.TextField(max_length=200, null=False, blank=True, db_index=True)
+    description = models.CharFieldField(max_length=200, null=False, blank=True, db_index=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
